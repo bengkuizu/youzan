4762ccb9cafa5d247dd8f2fbee95415307a5e902
xiaoming <123456@qq.com>
2019-11-07 17:29:41 +0800

    7号下午五点半

---
+++ src/views/customer/Customer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="all">
+    
+    <NavBar></NavBar>
+	<MainCenter></MainCenter>
+    <MainRight></MainRight>
+    <Add></Add>
+    <MessAge></MessAge>
+    </div>
+</template>
+<script>
+import NavBar from "@/components/NavBar.vue"
+import MainCenter from './MainCenter.vue'
+import MainRight from './MainRight.vue'
+import MessAge from './MessAge.vue'
+
+export default {
+    components:{
+        NavBar,
+        MainCenter,
+        MainRight,
+        Add,
+		MessAge
+    }
+}
+</script>
+<style lang="scss" scoped>
+.all{
+	background: #f7f8fa;
+}
+</style>
